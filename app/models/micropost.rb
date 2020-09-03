716fa41b7fc3570ae6b024c01ba4244866c2b3e0
tomo.tomo <tomotomo@gmail.com>
2020-09-03 01:22:27 +0000

    dependent: :destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,4 +5,5 @@ class Micropost < ApplicationRecord
   
   has_many :favorites
   has_many :users, through: :favorites
+  has_many :favorite, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_many :followers, through: :reverses_of_relationships, source: :user
   has_many :favorites
   has_many :favorite_microposts, through: :favorites, source: :micropost
+  has_many :favorite, dependent: :destroy
   
   def follow(other_user)
     unless self == other_user
